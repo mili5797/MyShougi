fdbaee197606069c70a1704c6b1d7feaf46d91cf
SaCat <mili57975@gmail.com>
2016-06-08 18:39:13 +0800

    Add some data operator of data class Move
    Add definition of set and get operator of data class move.

--- koma.h
+++ koma.h
@@ -14,18 +14,28 @@ typedef enum _dir{Dir_Forward,Dir_RFront,Dir_Right,Dir_RBack,Dir_Backward,Dir_LB
 typedef class _koma_move{
 public:
     _koma_move();
+    _koma_move& set_move(int x,int y);
+    _koma_move& set_if(int num,...);
+    unsigned int get_num_if();
+    int get_move_x();
+    int get_move_y();
+    std::vector<int> get_if_x();
+    std::vector<int> get_if_y();
 private:
     int x,y;
-    std::vector<int> if_x,if_y;
+    std::vector<int> if_x,if_y; //if there(if_x,if_y) is koma then can't move
 }Move; //data class
 
 typedef class _koma{
 public:
     _koma();
+    _koma& promote();
 protected:
     static int total_num;
     Where where;
     Owner owner;
+    bool can_promotion;
+    bool promotion;
 }Koma; //abstract class of koma
 
 typedef class _rush_koma:Koma{
@@ -41,6 +51,5 @@ public:
 protected:
      std::vector<Move> can_go;
 }Jump; //abstract class of koma which using jump to move
-
 
 #endif // __KOMA_H_
