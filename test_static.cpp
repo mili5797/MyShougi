553cf062b43ca1805b4d7f721627e30feed1d3fe
SaCat <mili57975@gmail.com>
2016-06-09 18:47:09 +0800

    Reorganize parent_class
    Reorganize parent_class
    And add a function add_num()

--- test_static.cpp
+++ test_static.cpp
@@ -2,22 +2,15 @@
 
 class test_static_parent_class{
 public:
-    test_static_parent_class();
+    test_static_parent_class(){test_static_parent_class::add_num();};
+    static int add_num();
     static int get_num();
 private:
     static int num;
 };
-
 int test_static_parent_class::num=0;
-int test_static_parent_class::get_num()
-{
-    return test_static_parent_class::num;
-}
-
-test_static_parent_class::test_static_parent_class()
-{
-    num++;
-}
+int test_static_parent_class::add_num(){return num++;}
+int test_static_parent_class::get_num(){return num;}
 
 int test_static()
 {
@@ -26,6 +19,7 @@ int test_static()
     std::cout<<test_static_parent_class::get_num()<<std::endl;
     class test_static_parent_class class_f_b,class_f_c;
     std::cout<<test_static_parent_class::get_num()<<std::endl;
+
 
     return 0;
 }
