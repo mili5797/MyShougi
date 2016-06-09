60bf243dc9ae9ed03c2fd06188819469ab75b820
SaCat <mili57975@gmail.com>
2016-06-09 18:50:38 +0800

    Add two sub class
    Add tow sub class.
    Output is interesting!
    It's useful for our project~

--- test_static.cpp
+++ test_static.cpp
@@ -12,14 +12,50 @@ int test_static_parent_class::num=0;
 int test_static_parent_class::add_num(){return num++;}
 int test_static_parent_class::get_num(){return num;}
 
+class test_static_child_class_a:public test_static_parent_class{
+public:
+    test_static_child_class_a(){test_static_child_class_a::add_num();}
+    static int add_num();
+    static int get_num();
+private:
+    static int num;
+};
+int test_static_child_class_a::num=0;
+int test_static_child_class_a::add_num(){return num++;}
+int test_static_child_class_a::get_num(){return num;}
+
+class test_static_child_class_b:public test_static_parent_class{
+public:
+    test_static_child_class_b(){test_static_parent_class::add_num();}
+    static int get_num();
+private:
+    static int num;
+};
+int test_static_child_class_b::num=0;
+int test_static_child_class_b::get_num(){return num;}
+
 int test_static()
 {
-    std::cout<<test_static_parent_class::get_num()<<std::endl;
     class test_static_parent_class class_f_a;
     std::cout<<test_static_parent_class::get_num()<<std::endl;
-    class test_static_parent_class class_f_b,class_f_c;
+    class test_static_child_class_a class_c_a;
     std::cout<<test_static_parent_class::get_num()<<std::endl;
-
+    std::cout<<test_static_child_class_a::get_num()<<std::endl;
+    std::cout<<test_static_child_class_b::get_num()<<std::endl;
+    class test_static_child_class_b class_c_b;
+    std::cout<<test_static_parent_class::get_num()<<std::endl;
+    std::cout<<test_static_child_class_a::get_num()<<std::endl;
+    std::cout<<test_static_child_class_b::get_num()<<std::endl;
 
     return 0;
 }
+/*
+output:
+1
+2
+1
+0
+4
+1
+0
+*/
