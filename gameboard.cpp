4c9947a09b4ae9026326eb9914c7e9ef5bb11d7a
SaCat <mili57975@gmail.com>
2016-06-10 17:32:29 +0800

    Initial some definition of class of gameboard
    Initial definition of class of small, midium, and normal size of
    gameboard.

--- gameboard.cpp
+++ gameboard.cpp
@@ -1,1 +1,1 @@
-
+#include "gameboard.h"
--- gameboard.h
+++ gameboard.h
@@ -1,1 +1,41 @@
+#ifndef __GAMEBOARD_H_
+#define __GAMEBOARD_H_
 
+typedef class _gameboard{
+public:
+    _gameboard();
+protected:
+    unsigned int x,y;
+private:
+
+} Gameboard;
+
+typedef class _small_gameboard:public _gameboard{
+public:
+
+protected:
+
+private:
+
+}SGameboard;
+
+typedef class _midium_gameboard:public _gameboard{
+public:
+
+protected:
+
+private:
+
+}MGameboard;
+
+typedef class _normal_gameboard:public _gameboard{
+public:
+
+protected:
+
+private:
+
+}NGameboard;
+typedef class _normal_gameboard LGameboard;
+
+#endif // __GAMEBOARD_H_
