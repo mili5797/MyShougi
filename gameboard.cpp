1de0762c85bbd42dc1e206225162f938cb87eaf6
SaCat <mili57975@gmail.com>
2016-06-10 18:14:27 +0800

    Implement SGameboard create operator
    Implement SGameboard create operator.
    But we have a problem, that we don't know what koma it is easily because
    koma haven't infomation for this.
    After we need to add some code to tag each koma that what it is.

--- gameboard.cpp
+++ gameboard.cpp
@@ -1,1 +1,56 @@
 #include "gameboard.h"
+
+_gameboard::_gameboard()
+{
+    this->x=0;
+    this->y=0;
+}
+
+_gameboard& _gameboard::initial_board()
+{
+    if(this->x<=0||this->y<=0)
+    {
+        throw "bad x or y in initial_board";
+    }
+
+    this->board.resize(this->x);
+    for(unsigned int i=0;i<this->x;i++)
+    {
+        this->board[i].resize(this->y);
+    }
+
+    return *this;
+}
+
+
+_small_gameboard::_small_gameboard()
+{
+    this->x=3;
+    this->y=4;
+    this->initial_board();
+
+    this->koma_list.push_back(new King);
+    this->koma_list.push_back(new King);
+    this->koma_list.push_back(new Elephant);
+    this->koma_list.push_back(new Elephant);
+    this->koma_list.push_back(new Giraffe);
+    this->koma_list.push_back(new Giraffe);
+    this->koma_list.push_back(new Pawn);
+    this->koma_list.push_back(new Pawn);
+}
+
+
+_midium_gameboard::_midium_gameboard()
+{
+    this->x=5;
+    this->y=6;
+    this->initial_board();
+}
+
+
+_normal_gameboard::_normal_gameboard()
+{
+    this->x=9;
+    this->y=9;
+    this->initial_board();
+}
--- gameboard.h
+++ gameboard.h
@@ -1,18 +1,24 @@
 #ifndef __GAMEBOARD_H_
 #define __GAMEBOARD_H_
 
+#include <vector>
+#include "AnimalShougiSkoma.h"
+
 typedef class _gameboard{
 public:
     _gameboard();
+    _gameboard& initial_board();
 protected:
     unsigned int x,y;
+    std::vector<std::vector<int> > board;
+    std::vector<Koma> koma_list;
 private:
 
 } Gameboard;
 
 typedef class _small_gameboard:public _gameboard{
 public:
-
+    _small_gameboard();
 protected:
 
 private:
@@ -21,7 +27,7 @@ private:
 
 typedef class _midium_gameboard:public _gameboard{
 public:
-
+    _midium_gameboard();
 protected:
 
 private:
@@ -30,7 +36,7 @@ private:
 
 typedef class _normal_gameboard:public _gameboard{
 public:
-
+    _normal_gameboard();
 protected:
 
 private:
