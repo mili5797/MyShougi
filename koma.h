48d0f35c4a11bf89019e6726034973f11c3a4f66
SaCat <mili57975@gmail.com>
2016-06-15 17:35:30 +0800

    Add all koma name of shougi into Name
    Add all koma name of shougi into Name.
    Include name in English, in Japanese, in Jap in short, in notation in
    Eng, and in Animal shougi.

--- koma.h
+++ koma.h
@@ -14,7 +14,23 @@
 typedef enum _where{on_table=0,in_hand,now_hidden,where_total} Where;
 typedef enum _owner{pub=0,player1,player2,owner_total=NUM_PLAYER+1} Owner;
 typedef enum _dir{Dir_Forward=0,Dir_RFront,Dir_Right,Dir_RBack,Dir_Backward,Dir_LBack,Dir_Left,Dir_LFront,Dir_total} Direct;
-typedef enum _koma_name{Koma_King=0,Koma_Elephant,Koma_Giraffe,Koma_Pawn} Name;
+typedef enum _koma_name{Koma_King  = 0,Koma_Oushou  = 0,Koma_Ou  = 0,Koma_K  = 0,Koma_Lion=0     ,
+                                                                                 Koma_Elephant=1 ,
+                                                                                 Koma_Giraffe=2  ,
+                        Koma_Pawn  = 3,Koma_Fuhyou  = 3,Koma_Fu  = 3,Koma_P  = 3,Koma_Chick=3    ,
+                        Koma_pPawn = 4,Koma_Tokin   = 4,Koma_To  = 4,Koma_pP = 4,Koma_Chicken=4  ,
+                        Koma_Gold  = 5,Koma_Kinshou = 5,Koma_Kin = 5,Koma_G  = 5,
+                        Koma_Silver= 6,Koma_Ginshou = 6,Koma_Gin = 6,Koma_S  = 6,
+                        Koma_pSilver=7,Koma_Narigin = 7,Koma_Zen = 7,Koma_pS = 7,
+                        Koma_Knight= 8,Koma_Keima   = 8,Koma_Kei = 8,Koma_N  = 8,
+                        Koma_pKnight=9,Koma_Narikei = 9,             Koma_pN = 9,
+                        Koma_Lance =10,Koma_Kyousha =10,Koma_Kyou=10,Koma_L  =10,
+                        Koma_pLance=11,Koma_Narikyou=11,Koma_An  =11,Koma_pL =11,
+                        Koma_Rock  =12,Koma_Hisha   =12,Koma_Hi  =12,Koma_R  =12,
+                        Koma_Dragon=13,Koma_Ryuuou  =13,Koma_Ryuu=13,Koma_pR =13,
+                        Koma_Bishop=14,Koma_Kakugyou=14,Koma_Kaku=14,Koma_B  =14,
+                        Koma_Horse =15,Koma_Ryuuma  =15,Koma_Uma =15,Koma_pB =15,
+Name_total} Name;
 
 typedef class _koma_move{
 public:
